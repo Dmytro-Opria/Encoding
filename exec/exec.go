c569aa44171bfba2c7cac8cd8b8973051d32b2d4
Dmitriy-Opria <demoni421@gmail.com>
2017-09-07 14:59:16 +0300

    update

--- exec/exec.go
+++ exec/exec.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os/exec"
 	"os"
-	"bufio"
 )
 
 
@@ -14,28 +13,16 @@ func main(){
 func CreateWCfile(fileName string)(err error){
 	cmd := exec.Command("wc", "-l", "exec.go")
 
-	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		os.Exit(1)
-		return
-	}
-	scanner := bufio.NewScanner(cmdReader)
 	file, err := os.Create(fileName)
+
+	cmd.Stdout = file
+
+	cmd.Run()
 
 	if err != nil {
 		return
 	}
 	defer file.Close()
-
-	cmd.Start()
-
-	value := []byte{}
-
-	for scanner.Scan() {
-		value = append(value, scanner.Bytes()...)
-	}
-
-	file.Write(value)
 
 	return nil
 }
