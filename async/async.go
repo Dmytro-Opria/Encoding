04a1c2a0673036fbf50e3e0a3341e451156837de
Dmitriy-Opria <demoni421@gmail.com>
2017-09-07 16:46:12 +0300

    async

---
+++ async/async.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var workChan = make(chan int, 10)
+
+func main(){
+	for i := 1; i <= 10; i++ {
+		go worker(i)
+	}
+	printNumber()
+}
+
+func worker (inx int){
+	for {
+		toPrint(inx)
+	}
+}
+
+func toPrint(inx int){
+	workChan <- inx
+}
+func printNumber(){
+	for {
+		number := <- workChan
+		fmt.Println(number)
+		time.Sleep(1 * time.Second)
+	}
+}
