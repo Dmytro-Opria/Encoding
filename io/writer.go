e928a5957593ecf32a426b328cce6668451e0f77
Dmitriy-Opria <demoni421@gmail.com>
2017-09-08 12:46:42 +0300

    update

--- io/writer.go
+++ io/writer.go
@@ -1,19 +1,48 @@
 package main
 
 import (
-	_"io"
 	"fmt"
+	"io"
+	"os"
 )
 
 type Writer struct {
 }
 
+type FileWrite struct {
+	filename string
+}
+
 func main() {
+	path := "test.txt"
 	w := Writer{}
-	w.Write([]byte("Byte array"))
+	fw := FileWrite{filename:"file.txt"}
+	//w.Write([]byte("Byte array"))
+	//fmt.Fprintln(w,"ABCDEFGEREDFDF")
+	//io.Copy(w, strings.NewReader("ABCDEFGEREDFDF"))
+	file, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Println("Can not open file", path, err)
+	}
+	defer file.Close()
+	mwriter := io.MultiWriter(w, fw)
+	io.Copy(mwriter, file)
 }
 
 func(w Writer) Write(p []byte) (n int, err error){
 	fmt.Println(p)
+
+	return len(p), nil
+}
+
+func(w FileWrite) Write(p []byte) (n int, err error){
+	file, err := os.Create(w.filename)
+	if err != nil {
+		fmt.Println("Can not open file", w.filename, err)
+	}
+	defer file.Close()
+
+	file.Write(p)
+
 	return len(p), nil
 }
