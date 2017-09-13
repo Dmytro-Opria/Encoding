62dbca83db46fc38f8de2bd40570c18d71fa356e
Dmitriy-Opria <demoni421@gmail.com>
2017-09-13 17:31:41 +0300

    update

--- tcp/tcp_client/tcp_client.go
+++ tcp/tcp_client/tcp_client.go
@@ -5,27 +5,76 @@ import (
 	"bufio"
 	"os"
 	"fmt"
-	"time"
 )
 
 
 func main() {
 	conn, _ := net.Dial("tcp", "127.0.0.1:3001")
 	reader := bufio.NewReader(os.Stdin)
+
 		for {
 			fmt.Print("Text to send: ")
-			text, _ := reader.ReadString('\n')
+			text, err := reader.ReadString('\n')
+
+			if err != nil {
+				fmt.Println("Can`t read string", err)
+			}
 
 			fmt.Fprintf(conn, text)
 
-			message, _, _ := bufio.NewReader(conn).ReadLine()
-			if len(message) < 7 {
-				time.Sleep(time.Second/50)
-				continue
+			buf := make([]byte, len([]byte(text))+6)
+			conn.Read(buf)
+
+			if err != nil {
+				fmt.Println("Can`t read line", err)
 			}
-			fmt.Print("Message from server: " + string(message)+"\n")
+			fmt.Print("Message from server: " + string(buf))
+		}
+}
+/*
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:3001")
+	reader := bufio.NewReader(os.Stdin)
+
+	proto := textproto.NewReader(bufio.NewReader(conn))
+	for {
+		fmt.Print("Text to send: ")
+		text, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Println("Can`t read string", err)
 		}
 
+		fmt.Fprintf(conn, text)
+		line, err :=  proto.ReadLine()
+
+		if err != nil {
+			fmt.Println("Can`t read line", err)
+		}
+		fmt.Print("Message from server: " + line  +"\n")
+	}
+}*/
+
+/*
+
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:3001")
+	reader := bufio.NewReader(os.Stdin)
+	message, _, _ := bufio.NewReader(conn).ReadLine()
+
+	for {
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+
+		fmt.Fprintf(conn, text)
+		if len(message) < 7 {
+			time.Sleep(time.Second/50)
+			continue
+		}
+		fmt.Print("Message from server: " + string(message)+"\n")
+	}
+
 }
+*/
 
 
