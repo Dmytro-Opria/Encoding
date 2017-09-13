00b544e48858188123604e4d452076ae06039e93
Dmitriy-Opria <demoni421@gmail.com>
2017-09-13 17:15:14 +0300

    update

--- tcp/tcp_client/tcp_client.go
+++ tcp/tcp_client/tcp_client.go
@@ -8,18 +8,24 @@ import (
 	"time"
 )
 
+
 func main() {
 	conn, _ := net.Dial("tcp", "127.0.0.1:3001")
-	for {
+	reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Print("Text to send: ")
+			text, _ := reader.ReadString('\n')
 
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
+			fmt.Fprintf(conn, text)
 
-		fmt.Fprintf(conn, text + "\n")
+			message, _, _ := bufio.NewReader(conn).ReadLine()
+			if len(message) < 7 {
+				time.Sleep(time.Second/50)
+				continue
+			}
+			fmt.Print("Message from server: " + string(message)+"\n")
+		}
 
-		time.Sleep(time.Second)
-		message, _, _ := bufio.NewReader(conn).ReadLine()
-		fmt.Print("Message from server: " + string(message)+"\n")
-	}
 }
+
+
